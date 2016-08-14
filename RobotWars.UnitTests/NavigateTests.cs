4366079ac275492f7ac5da14cffca1b0ec5aa439
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-08-14 17:08:01 +0100

    Implemented remainining two test scenarios All tests passing
    Removed TODO
    
    Removed commented scenarios now all are implemented as tests

--- RobotWars.UnitTests/NavigateTests.cs
+++ RobotWars.UnitTests/NavigateTests.cs
@@ -22,7 +22,6 @@ namespace RobotWars.UnitTests
             Heading expectedHeading = new Heading("N");
             int expectedPenalties = 0;
 
-            //TODO ADC: consider using a Queue (FIFO)
             string movementInstructions = "MLMRMMMRMMRR";
 
             BattleArena battleArena = new BattleArena();
@@ -55,7 +54,6 @@ namespace RobotWars.UnitTests
             Heading expectedHeading = new Heading("W");
             int expectedPenalties = 1;
 
-            //TODO ADC: consider using a Queue (FIFO)
             string movementInstructions = "LMLLMMLMMMRMM";
 
             BattleArena battleArena = new BattleArena();
@@ -77,38 +75,69 @@ namespace RobotWars.UnitTests
             Assert.That(robot.Heading.Value, Is.EqualTo(expectedHeading.Value));
             Assert.That(robot.Penalties, Is.EqualTo(expectedPenalties));
         }
+
+        [Test]
+        public void Given_Initial_Location_Of_22W_When_Instructions_Are_MLMLMLMRMRMRMRM_Then_Final_Location_And_Penalty_Count_Is_22N0()
+        {
+            Location initialLocation = new Location(2, 2);
+            Heading initialHeading = new Heading("W");
+
+            Location expectedLocation = new Location(2, 2);
+            Heading expectedHeading = new Heading("N");
+            int expectedPenalties = 0;
+
+            string movementInstructions = "MLMLMLMRMRMRMRM";
+
+            BattleArena battleArena = new BattleArena();
+            Robot robot = new Robot(battleArena);
+
+            battleArena.PlaceRobot(robot, initialLocation, initialHeading);
+
+            Trace.WriteLine(robot.CurrentStatus);
+
+            foreach (char instruction in movementInstructions)
+            {
+                robot.Navigate(instruction);
+
+                Trace.WriteLine(robot.CurrentStatus);
+            }
+
+            Assert.That(robot.Location.X, Is.EqualTo(expectedLocation.X));
+            Assert.That(robot.Location.Y, Is.EqualTo(expectedLocation.Y));
+            Assert.That(robot.Heading.Value, Is.EqualTo(expectedHeading.Value));
+            Assert.That(robot.Penalties, Is.EqualTo(expectedPenalties));
+        }
+
+        [Test]
+        public void Given_Initial_Location_Of_13N_When_Instructions_Are_MMLMMLMMMMM_Then_Final_Location_And_Penalty_Count_Is_00S3()
+        {
+            Location initialLocation = new Location(1, 3);
+            Heading initialHeading = new Heading("N");
+
+            Location expectedLocation = new Location(0, 0);
+            Heading expectedHeading = new Heading("S");
+            int expectedPenalties = 3;
+
+            string movementInstructions = "MMLMMLMMMMM";
+
+            BattleArena battleArena = new BattleArena();
+            Robot robot = new Robot(battleArena);
+
+            battleArena.PlaceRobot(robot, initialLocation, initialHeading);
+
+            Trace.WriteLine(robot.CurrentStatus);
+
+            foreach (char instruction in movementInstructions)
+            {
+                robot.Navigate(instruction);
+
+                Trace.WriteLine(robot.CurrentStatus);
+            }
+
+            Assert.That(robot.Location.X, Is.EqualTo(expectedLocation.X));
+            Assert.That(robot.Location.Y, Is.EqualTo(expectedLocation.Y));
+            Assert.That(robot.Heading.Value, Is.EqualTo(expectedHeading.Value));
+            Assert.That(robot.Penalties, Is.EqualTo(expectedPenalties));
+        }
     }
-
-
-
-
-
-    /*
-
-Initial Position
-Movements instructions
-Final position and penalty count
-
-    Scenario 2
-4, 4, S
-LMLLMMLMMMRMM
-Position:	0, 1, W
-Penalties: 	1
-
-    Scenario 3
-2, 2, W
-MLMLMLM RMRMRMRM
-Position:	2, 2, N
-Penalties: 	0
-
-    Scenario 4
-1, 3, N
-MMLMMLMMMMM
-Position:	0, 0, S
-Penalties: 	3
-
-
-
-    */
-
 }
